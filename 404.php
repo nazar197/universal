7971e41312e46bb776032758fc6b8445851cb84c
Nazar Pidhrushnyi <pidgrushnyi19@gmail.com>
2021-02-28 15:15:43 +0200

    init: create own wp theme

---
+++ 404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+
+<div class="h1-wrapper page-404">
+  <h1>This is 404 page!</h1>
+</div>
+
+<?php get_footer();
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+
+<div class="h1-wrapper">
+  <h1>Hello, world!</h1>
+</div>
+
+<?php get_footer();
